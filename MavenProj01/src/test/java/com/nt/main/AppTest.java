b3087ce7ee81025f0abc953235d98e267ac554f4
Akhil Reddy <akhilreddynagulapally.nle@gmail.com>
2025-10-13 16:05:24 -0700

    mavrn session 6 completed

--- MavenProj01/src/test/java/com/nt/main/AppTest.java
+++ MavenProj01/src/test/java/com/nt/main/AppTest.java
@@ -1,8 +1,10 @@
 package com.nt.main;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+
+import com.nt.service.Arithmetic;
 
 /**
  * Unit test for simple App.
@@ -13,7 +15,50 @@ public class AppTest {
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testSumWithPositiveValues() {
+    	int x=10;
+    	int y=20;
+    	int expected=30;
+    	//final result
+    	Arithmetic ar=new Arithmetic();
+    	int actual=ar.sum(x,y);
+    	assertEquals(expected,actual);
+    	
+    }
+    
+    @Test
+    public void testSumWithNegativeValues() {
+    	int x=-10;
+    	int y=-20;
+    	int expected=-30;
+    	//final result
+    	Arithmetic ar=new Arithmetic();
+    	int actual=ar.sum(x,y);
+    	assertEquals(expected,actual);
+    	
+    }
+    
+    @Test
+    public void testSumWithMixedValues() {
+    	int x=10;
+    	int y=-20;
+    	int expected=10;
+    	//final result
+    	Arithmetic ar=new Arithmetic();
+    	int actual=ar.sum(x,y);
+    	assertEquals(expected,actual);
+    	
+    }
+    
+    @Test
+    public void testSumWithZeros() {
+    	int x=0;
+    	int y=0;
+    	int expected=0;
+    	//final result
+    	Arithmetic ar=new Arithmetic();
+    	int actual=ar.sum(x,y);
+    	assertEquals(expected,actual);
+    	
     }
 }
